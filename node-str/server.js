8e471e08b41981455197bf8377cc5548025daccd
cyntiagoulart <cyntiartg@gmail.com>
2020-10-04 20:44:06 -0300

    Primeiro commit do projeto para adicionar o servidor, o package.json e o .gitignore.

---
+++ node-str/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const http = require('http');
+const debug = require('debug')('nodestr: server');
+const express = require('express');
+
+const app = express();
+const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+	res.status(200).send({
+		title: 'Node Store API',
+		version: '0.0.1',
+	});
+});
+
+app.use('/', route);
+
+server.listen(port);
+console.log(`API rodando na porta ${port}`);
+console.log(`Digite CTRL + C para parar o servidor.`);
+
+function normalizePort(val) {
+	const port = parseInt(val, 10);
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+
+	return false;
+}
