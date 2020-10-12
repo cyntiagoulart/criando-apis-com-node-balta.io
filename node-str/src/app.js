ae1dfe4e710c5b91398b6e8a080f1805f1362555
cyntiagoulart <cyntiartg@gmail.com>
2020-10-12 14:04:58 -0300

    Criação do CRUD, rotas, controllers, models, instalação do Mongoose e conexão com banco de dados MongoDB Atlas.

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -2,40 +2,23 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+//conecta o banco de dados
+mongoose.connect('mongodb+srv://admin:Passw0rd67@cluster0.16t7r.mongodb.net/geral?retryWrites=true&w=majority');
+
+
+// Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-	res.status(200).send({
-		title: 'Node Store API',
-		version: '0.0.1',
-	});
-});
-
-const create = router.post('/', (req, res, next) => {
-	res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-	const id = req.params.id;
-	res.status(200).send({
-		id: id,
-		item: req.body
-	});
-});
-
-const del = router.delete('/', (req, res, next) => {
-	res.status(200).send(req.body);
-});
-
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
